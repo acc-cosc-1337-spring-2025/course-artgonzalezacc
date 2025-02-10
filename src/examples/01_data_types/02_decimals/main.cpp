4f32cf29b312f4b2eb182789a69e5dca079dcf73
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2025-02-10 23:43:30 +0000

    decimal comparisons not reliable

--- src/examples/01_data_types/02_decimals/main.cpp
+++ src/examples/01_data_types/02_decimals/main.cpp
@@ -1,5 +1,12 @@
+#include "decimals.h"
+
+using std::cout;
+
 int main()
 {
+    cout<<"Double(decimal) comparisons not reliable: ";
+    double result = add_to_double_1(0);
+    cout<<"Output: "<<result<<"\n";
 
     return 0;
 }
