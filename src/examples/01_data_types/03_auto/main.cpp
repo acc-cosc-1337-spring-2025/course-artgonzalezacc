55c249334e45c83d682b9bd15752dc3c32300a10
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2025-02-11 00:03:50 +0000

    use of auto

--- src/examples/01_data_types/03_auto/main.cpp
+++ src/examples/01_data_types/03_auto/main.cpp
@@ -1,5 +1,17 @@
+#include<iostream>
+
+using std::cout;
+
 int main()
 {
+    auto letter = 'a';
+    auto num_int = 10;
+    auto num_double = 10.5;
+
+    cout<<letter<<"\n";
+    cout<<num_int<<"\n";
+    cout<<num_double<<"\n";
+
 
     return 0;
 }
