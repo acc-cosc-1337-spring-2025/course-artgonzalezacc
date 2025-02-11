2795eeb63c303378c6c03b47f4e424c28e796e47
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2025-02-11 00:29:08 +0000

    create a string

--- src/examples/01_data_types/05_string/main.cpp
+++ src/examples/01_data_types/05_string/main.cpp
@@ -1,7 +1,15 @@
+#include<iostream>
+#include<string>
+
+using std::cout;
+using std::string;
 
 //create string variable, initialize and display
 int main()
 {
+    string lang = "C++";
+    
+    cout<<lang<<"\n";
 
     return 0;
 }
