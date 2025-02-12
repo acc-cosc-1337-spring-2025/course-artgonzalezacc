a51e0b254748615e71cfa8dc1ec7e0ed3ba4068e
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2025-02-12 23:52:49 +0000

    op precedence 1 and 2

--- src/examples/02_expressions/01_expressions/expressions.cpp
+++ src/examples/02_expressions/01_expressions/expressions.cpp
@@ -23,6 +23,10 @@ them with the third
 @param int num3
 @return (num1 + num2) / num3
 */
+int operator_precedence_2(int num1, int num2, int num3)
+{
+    return (num1 + num2) / num3;
+}
 
 
 /*
--- test/examples/02_expressions/02_expressions_tests.cpp
+++ test/examples/02_expressions/02_expressions_tests.cpp
@@ -11,3 +11,9 @@ TEST_CASE("Verify operator precedence 1")
 	REQUIRE(operator_precedence_1(2, 6, 3) == 4);
 	REQUIRE(operator_precedence_1(2, 10, 2) == 7);
 }
+
+TEST_CASE("Verify operator precedence 2")
+{
+	REQUIRE(operator_precedence_2(2, 6, 2) == 4);
+	REQUIRE(operator_precedence_2(2, 10, 2) == 6);
+}
