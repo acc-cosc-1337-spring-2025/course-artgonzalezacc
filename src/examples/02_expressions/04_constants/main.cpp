68b721caf519925d1563a6e9110a0219fdf762bc
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2025-02-13 00:25:06 +0000

    use consant in main

--- src/examples/02_expressions/04_constants/main.cpp
+++ src/examples/02_expressions/04_constants/main.cpp
@@ -1,5 +1,21 @@
+#include<iostream>
+#include "constants.h"
+
+using std::cin;
+using std::cout;
 
 int main()
 {
+    cout<<RATE<<"\n";
+    //RATE = .99; this generates a compile error .. syntax -> binary -> executable
+
+    auto amount = 0;
+
+    cout<<"Enter amount: ";
+    cin>>amount;
+
+    double result = calculat_interest(amount);
+    cout<<"Result: "<<result<<"\n";
+
     return 0;
 }
