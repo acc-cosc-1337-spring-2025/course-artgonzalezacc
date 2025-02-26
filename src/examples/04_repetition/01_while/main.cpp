406668936d39bdd17583109b066b28d8a7f5a1dd
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2025-02-26 23:36:05 +0000

    validate data w while

--- src/examples/04_repetition/01_while/main.cpp
+++ src/examples/04_repetition/01_while/main.cpp
@@ -6,8 +6,11 @@ int main()
 {
 	auto num = 0;
 
-	cout<<"Enter a number: ";
-	cin>>num;
+	while(num < 1 || num > 10)
+	{
+		cout<<"Enter a number between 1 and 10: ";
+		cin>>num;
+	}
 
 	auto result = get_sum_of_squares(num);
 	cout<<"Sum of squares: "<<result<<"\n";
