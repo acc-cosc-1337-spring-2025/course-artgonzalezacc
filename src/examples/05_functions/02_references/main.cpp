8b08d6416c1bd4836763382337fff7e449654173
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2025-02-27 00:17:27 +0000

    intro to reference(memory) operator

--- src/examples/05_functions/02_references/main.cpp
+++ src/examples/05_functions/02_references/main.cpp
@@ -1,4 +1,16 @@
+#include<iostream>
+
+using std::cout;
+
 int main()
 {
+    auto num = 10, num1 = 20;
+
+    cout<<"Num value: "<<num<<"\n";
+    cout<<"Num variable is at address: "<<&num<<"\n";//memory operator &
+
+    cout<<"Num1 value: "<<num1<<"\n";
+    cout<<"Num1 variable is at address: "<<&num1<<"\n";
+
     return 0;
 }
