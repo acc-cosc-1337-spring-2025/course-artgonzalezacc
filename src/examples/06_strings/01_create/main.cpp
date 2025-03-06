8156a2fcb7d4253d491a277aeafab523027c609a
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2025-03-06 00:24:57 +0000

    create string, size, and capacity

--- src/examples/06_strings/01_create/main.cpp
+++ src/examples/06_strings/01_create/main.cpp
@@ -1,4 +1,28 @@
+#include<iostream>
+#include<string>
+
+using std::cout;
+using std::string;
+
 int main()
 {
+    string lang = "C++";
+
+    cout<<lang<<"\n";
+    cout<<"size: "<<lang.size()<<"\n";
+    cout<<"capacity: "<<lang.capacity()<<"\n\n";
+
+    lang.append("123456789012");
+
+    cout<<lang<<"\n";
+    cout<<"size: "<<lang.size()<<"\n";
+    cout<<"capacity: "<<lang.capacity()<<"\n\n";
+
+    lang.append("1");
+
+    cout<<lang<<"\n";
+    cout<<"size: "<<lang.size()<<"\n";
+    cout<<"capacity: "<<lang.capacity()<<"\n";
+
     return 0;
 }
