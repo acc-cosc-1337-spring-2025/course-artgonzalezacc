9db7b78abe20e7a16f6bede1075a3203d465683f
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2025-03-10 23:17:55 +0000

    string value be careful w reference op

---
+++ src/examples/06_strings/02_functions/function_str.cpp
@@ -0,0 +1,16 @@
+#include "function_str.h"
+
+using std::cout;
+using std::string;
+
+void string_value_param(string lang)
+{
+    for(auto &ch: lang) //no & means working with a copy of each lang character
+    {
+        ch = '.';
+    }
+
+    cout<<lang<<"\n";
+
+}
+
--- test/examples/06_strings/06_strings_tests.cpp
+++ test/examples/06_strings/06_strings_tests.cpp
@@ -1,6 +1,18 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "function_str.h"
+
+using std::string;
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
+}
+
+TEST_CASE("Test string value or copy param")
+{
+	string lang = "C++";
+
+	string_value_param(lang);
+
+	REQUIRE(lang == "C++");
 }
