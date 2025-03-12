b9c212ef06fe92431ff08fb49930b64ded4924e3
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2025-03-12 23:18:15 +0000

    vector memory addressing

--- src/examples/07_vector/02_functions/main.cpp
+++ src/examples/07_vector/02_functions/main.cpp
@@ -1,8 +1,19 @@
-#include "function_vec.h"
+#include<iostream>
+#include<vector>
+
+using std::cout;
+using std::vector;
 
 int main()
 {
-    loop_vector_w_for();
+    vector<int> nums {1, 5, 3};
+    auto num = 5;
 
-    return 0;
+    cout<<"Address of lang: "<<&nums<<"\n";
+    cout<<"Address of num: "<<&num<<"\n\n";
+
+    cout<<nums[0]<<"\n";
+    cout<<"Address of character at 0 index value 1: "<<&nums[0]<<"\n";
+    cout<<"Address of character at 1 index value 5: "<<&nums[1]<<"\n";
+    cout<<"Address of character at 2 index value 3: "<<&nums[2]<<"\n";
 }
