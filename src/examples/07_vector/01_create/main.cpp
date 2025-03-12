2efa0e5c982e0354571ec510c5c50968004797ed
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2025-03-12 22:51:05 +0000

    create vector, size and capacity

--- src/examples/07_vector/01_create/main.cpp
+++ src/examples/07_vector/01_create/main.cpp
@@ -1,4 +1,29 @@
+#include<iostream>
+#include<vector>
+
+using std::cout; using std::vector;
+
 int main()
 {
+    vector<int> nums{3, 7, 9};
+
+    cout<<"size: "<<nums.size()<<"\n";
+    cout<<"capacity: "<<nums.capacity()<<"\n\n";
+
+    nums.push_back(5);
+
+    cout<<"size: "<<nums.size()<<"\n";
+    cout<<"capacity: "<<nums.capacity()<<"\n\n";
+
+    nums.push_back(8);
+    nums.push_back(1);
+
+    cout<<"size: "<<nums.size()<<"\n";
+    cout<<"capacity: "<<nums.capacity()<<"\n\n";
+
+    nums.push_back(10);
+    cout<<"size: "<<nums.size()<<"\n";
+    cout<<"capacity: "<<nums.capacity()<<"\n\n";
+
     return 0;
 }
