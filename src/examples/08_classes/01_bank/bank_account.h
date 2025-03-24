6c5ef0240f4057609f78ac910f0c057dc6ba914c
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2025-03-24 23:29:45 +0000

    intro to class structure example-bank account

--- src/examples/08_classes/01_bank/bank_account.h
+++ src/examples/08_classes/01_bank/bank_account.h
@@ -1,1 +1,12 @@
 //bank_account.h
+
+class BankAccount 
+{
+//access specifier variables/functions    
+public://outside code class/functions can use variables and functions that are public
+    BankAccount(int b) : balance(b) {}//class constructor
+    int get_balance(){return balance;}
+private://only this class can use variables/functions
+    int balance {0};
+
+};
--- src/examples/08_classes/01_bank/main.cpp
+++ src/examples/08_classes/01_bank/main.cpp
@@ -1,6 +1,12 @@
+#include "bank_account.h"
+#include <iostream>
+
+using std::cout;
 
 int main()
 {
+	BankAccount account(500);//create our own variable from the newly created data type
+	cout<<"Balance: "<<account.get_balance()<<"\n";
 
 	return 0;
 }
