a60c22a3d491290fb808b32ef20a7290f609b45b
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2025-03-26 23:12:29 +0000

    Use BankAccount in main

--- src/examples/08_classes/01_bank/main.cpp
+++ src/examples/08_classes/01_bank/main.cpp
@@ -2,10 +2,23 @@
 #include <iostream>
 
 using std::cout;
+using std::cin;
 
 int main()
-{   //variable of a class is equal to  an object in memory
+{
+	auto amount = 0;
+	//variable of a class is equal to  an object in memory
 	BankAccount account(500);//create our own variable from the newly created data type
+	cout<<"Balance: "<<account.get_balance()<<"\n";
+
+	cout<<"Enter amount to deposit: ";
+	cin>>amount;
+	account.deposit(amount);
+	cout<<"Balance: "<<account.get_balance()<<"\n";
+
+	cout<<"Enter amount to withdraw: ";
+	cin>>amount;
+	account.withdraw(amount);
 	cout<<"Balance: "<<account.get_balance()<<"\n";
 
 	return 0;
