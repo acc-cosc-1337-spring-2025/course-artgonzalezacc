0c3a9022787b69789db249c79eadbf746ab96d0c
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2025-04-02 23:02:58 +0000

    using const in classes

--- src/examples/08_classes/01_bank/bank_account.h
+++ src/examples/08_classes/01_bank/bank_account.h
@@ -1,4 +1,5 @@
 //bank_account.h
+#include<iostream>
 
 #ifndef BANK_ACCOUNT_H
 #define BANK_ACCOUNT_H
@@ -8,7 +9,7 @@ class BankAccount
 //access specifier variables/functions    
 public://outside code class/functions can use variables and functions that are public
     BankAccount(int b) : balance(b) {}//class constructor
-    int get_balance(){return balance;}
+    int get_balance()const {std::cout<<"BankAccount:get_balance(): "; return balance;}
     void deposit(int amount);
     void withdraw(int amount);
 private://only this class can use variables/functions
