2961345464aac566f30d028df4298102daab03f5
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2025-04-02 23:16:41 +0000

    savings class overriding

--- src/examples/08_classes/01_bank/savings_account.h
+++ src/examples/08_classes/01_bank/savings_account.h
@@ -8,6 +8,7 @@ class SavingsAccount : public BankAccount
 {
 public:
     SavingsAccount(int b) : BankAccount(b){}
+    int get_balance()const {std::cout<<"SavingsAccount:get_balance: "; return BankAccount::get_balance();}
 private:
 
 };
