4484121189b4cf07acb01330d52da8d4188f5fc5
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2025-04-09 23:24:44 +0000

    weak ptr example

--- src/examples/11_memory/02_dynamic_memory/main.cpp
+++ src/examples/11_memory/02_dynamic_memory/main.cpp
@@ -2,16 +2,18 @@
 #include<memory>
 
 using std::cout; using std::shared_ptr; using std::make_shared;
+using std::weak_ptr;
 
 int main() 
 {
 	shared_ptr<int> num_sp1 = make_shared<int>(10);
-	cout<<*num_sp1<<"\n";
-	cout<<"Count: "<<num_sp1.use_count()<<"\n";
+	weak_ptr<int> num_wp = num_sp1;
 
-	shared_ptr<int> num_sp2 = num_sp1;
-	cout<<*num_sp2<<"\n";
-	cout<<"Count: "<<num_sp2.use_count()<<"\n";
+	if(!num_wp.expired())
+	{	shared_ptr<int> sp1 = num_wp.lock();
+
+		cout<<*sp1<<"\n";
+	}
 	
 
 
