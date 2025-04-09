6b06c53464eaebcc1b3a2a54bb79f5b50337b38a
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2025-04-09 22:41:30 +0000

    create dynamic memory(heap)

--- src/examples/11_memory/02_dynamic_memory/main.cpp
+++ src/examples/11_memory/02_dynamic_memory/main.cpp
@@ -1,5 +1,21 @@
+#include<iostream>
+
+using std::cout;
+
 int main() 
 {
-	
+	//Create memory
+	int* num_ptr = new int(10); //pointer with the keyword new ---> creates memory on the heap
+
+	//use the memory(FOR HOW LONG DO WE NEED THE DATA ON THE HEAP)
+	cout<<*num_ptr<<"\n";
+	//maybe lots of lines of code follow here; functions class/class function calls
+
+	//WE WILL FORGET TO CLEAR THE MEMORY
+
+	//free/delete the memory--->free up the address(es) that are used with new
+	delete num_ptr;
+	num_ptr = nullptr;//point to address 0
+
 	return 0;
 }
