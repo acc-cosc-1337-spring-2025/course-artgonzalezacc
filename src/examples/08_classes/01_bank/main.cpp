fb2f863abb7d7e098f947d3ea506d283ec23cb06
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2025-04-14 22:50:20 +0000

    unique ptr and std::move

--- src/examples/08_classes/01_bank/main.cpp
+++ src/examples/08_classes/01_bank/main.cpp
@@ -4,15 +4,21 @@
 #include "savings_account.h"
 #include <iostream>
 #include<memory>
+#include<vector>
 
 using std::cout; using std::unique_ptr; using std::make_unique;
-using std::cin;
+using std::cin; using std::vector;
 
 int main()
 {
-	unique_ptr<BankAccount> account = make_unique<SavingsAccount>(500);
+	vector<unique_ptr<BankAccount>> accounts;
+	unique_ptr<BankAccount> checking = make_unique<CheckingAccount>(500);
 
-	cout<<"Balance: "<<account->get_balance()<<"\n";
+	cout<<"checking get balance "<<checking->get_balance()<<"\n";
+
+	accounts.push_back(std::move(checking));
+	
+	cout<<"checking get balance "<<checking->get_balance()<<"\n";
 
 
 	return 0;
