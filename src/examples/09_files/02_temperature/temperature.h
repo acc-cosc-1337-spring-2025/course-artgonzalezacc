9522ace9fc80aa12b2bc8c0392f92fe81c67a88c
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2025-04-21 23:02:06 +0000

    add temperature class

--- src/examples/09_files/02_temperature/temperature.h
+++ src/examples/09_files/02_temperature/temperature.h
@@ -1,1 +1,16 @@
 //temperature.h
+#ifndef TEMPERATURE_H
+#define TEMPERATURE_H
+
+class Temperature
+{
+public:
+    Temperature(int h, double r) : hour(h), reading(r){/**empty code block */}
+    int get_hour() const{return hour;}
+    double get_temperature() const {return reading;}
+private:
+    int hour;
+    double reading;
+};
+
+#endif
