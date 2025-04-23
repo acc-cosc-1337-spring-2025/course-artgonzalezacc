40af6231c2405296e047f4b6184a9c0b7763c596
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2025-04-23 22:40:01 +0000

    stack array

--- src/examples/10_arrays/01_arrays_mem/arrays_mem.cpp
+++ src/examples/10_arrays/01_arrays_mem/arrays_mem.cpp
@@ -1,1 +1,14 @@
 //write code for for stack_array and display each element to screen
+#include "arrays_mem.h"
+
+using std::cout;
+
+void create_stack_array()
+{
+    const int SIZE = 3;
+    int nums[SIZE]{6, 1, 10};
+
+    cout<<nums[0]<<" "<<&nums[0]<<"\n";
+    cout<<nums[1]<<" "<<&nums[1]<<"\n";
+    cout<<nums[2]<<" "<<&nums[2]<<"\n";
+}
