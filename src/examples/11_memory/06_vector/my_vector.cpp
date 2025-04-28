c6207ac2e047453c8d6288fc121ad5fb372e1a9c
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2025-04-28 23:02:45 +0000

    my vector class destructor and constructor

--- src/examples/11_memory/06_vector/my_vector.cpp
+++ src/examples/11_memory/06_vector/my_vector.cpp
@@ -1,4 +1,15 @@
 #include "my_vector.h"
 
-//
+using std::cout;
+
+MyVector::MyVector(int c) : capacity(c), elements{new int[c]}
+{
+    cout<<"Constructor MyVector(c) created memory at: "<<elements<<"\n";
+}
+
+MyVector::~MyVector()
+{
+    cout<<"Destructor ~MyVector() deleting memory at: "<<elements<<"\n";
+    delete[] elements;
+}
 
--- src/examples/11_memory/06_vector/my_vector.h
+++ src/examples/11_memory/06_vector/my_vector.h
@@ -1,1 +1,18 @@
 //
+#include<iostream>
+
+#ifndef MY_VECTOR_H
+#define MY_VECTOR_H
+
+class MyVector
+{
+public:
+    MyVector(int c);//constructor
+    ~MyVector();//destructor
+private:
+    int size{0};//number of elements in the list
+    int capacity;//total number of elements
+    int* elements;
+};
+
+#endif
