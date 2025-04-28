1724edc0758bae60162042768f0300fc62949067
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2025-04-28 23:04:36 +0000

    operator overloading []

--- src/examples/11_memory/06_vector/main.cpp
+++ src/examples/11_memory/06_vector/main.cpp
@@ -4,6 +4,7 @@
 int main()
 {
     MyVector vector(3);
+    vector[3] = 0;
 
     return 0;
 }
--- src/examples/11_memory/06_vector/my_vector.h
+++ src/examples/11_memory/06_vector/my_vector.h
@@ -8,6 +8,8 @@ class MyVector
 {
 public:
     MyVector(int c);//constructor
+    int& operator[](int index){return elements[index];}
+    int& operator[](int index)const{return elements[index];}    
     ~MyVector();//destructor
 private:
     int size{0};//number of elements in the list
