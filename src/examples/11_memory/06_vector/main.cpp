6441ce347b2320e91f18080a1865b9372d58bb48
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2025-05-05 23:19:08 +0000

    myvector in main add and display elements

--- src/examples/11_memory/06_vector/main.cpp
+++ src/examples/11_memory/06_vector/main.cpp
@@ -1,10 +1,21 @@
 //
 #include "my_vector.h"
 
+using std::cout;
+
 int main()
 {
-    MyVector vector(3);
-    vector[3] = 0;
+    MyVector vector(5);
+    vector.PushBack(3);
+    vector.PushBack(7);
+    vector.PushBack(9);
+    vector.PushBack(77);
+    vector.PushBack(8);
+
+    for(auto i=0; i < vector.Size(); i++)
+    {
+        cout<<vector[i]<<"\n";
+    }
 
     return 0;
 }
