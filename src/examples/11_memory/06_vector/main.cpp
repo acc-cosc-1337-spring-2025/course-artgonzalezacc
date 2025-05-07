b7d8cad70bb7e0013e66caaf5459b4797a739539
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2025-05-07 22:38:48 +0000

    template functions

--- src/examples/11_memory/06_vector/main.cpp
+++ src/examples/11_memory/06_vector/main.cpp
@@ -1,22 +1,29 @@
 //
 #include "my_vector.h"
+#include<string>
 
-using std::cout;
+using std::cout; using std::string;
+
+template<class T>
+T add(T a, T b)
+{
+    return a + b;    
+}
 
 int main()
 {
-    MyVector vector(5);
-    vector.PushBack(3);
-    vector.PushBack(7);
-    vector.PushBack(9);
-    vector.PushBack(77);
-    vector.PushBack(8);
-    vector.PushBack(55);
+    auto result = add(5, 5);
+    cout<<result<<"\n";
 
-    for(auto i=0; i < vector.Size(); i++)
-    {
-        cout<<vector[i]<<"\n";
-    }
+    auto result1 = add(5.6, 6.8);
+    cout<<result1<<"\n";
+
+    string str1 = "c++";
+    string str2 = " is modern";
+    auto result2 = add(str1, str2);
+    
+    cout<<result2<<"\n";
+
 
     return 0;
 }
